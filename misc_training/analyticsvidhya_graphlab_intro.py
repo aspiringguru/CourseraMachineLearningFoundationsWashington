787596c09c3bb127eab55f4bc1ea26a2b7c7c951
aspiringguru <bmatthewtaylor@gmail.com>
2016-11-12 16:10:46 +1000

    working examples added - reccomender systems

---
+++ misc_training/analyticsvidhya_graphlab_intro.py
@@ -0,0 +1,7 @@
+import graphlab
+import pandas
+
+sf_test  = graphlab.SFrame('../data/black_friday/test.csv')
+sf_train = graphlab.SFrame('../data/black_friday/train.csv')
+
+print pandas.sf_train.head(3)
